4debc20c4dc68f1ab27e40f0794896225751ee03
AAlabs10 <Adeniranadetoyese25@gmail.com>
2025-09-10 00:39:57 -0700

    all

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
         </div>
 
         {/* Desktop Menu */}
-        <div className="hidden md:flex space-x-6">
+        <div className="hidden md:flex space-x-6 mr-4">
           <a href="#" className="text-gray-500 hover:text-yellow-500">Home</a>
           <a href="#" className="text-gray-500 hover:text-yellow-500">Shop</a>
           <a href="#" className="text-gray-500 hover:text-yellow-500">About</a>
@@ -21,7 +21,7 @@ const Navbar = () => {
 
     
         <div className="flex items-center space-x-4">
-          <div className="hidden md:flex items-center border rounded-full px-3 py-1 bg-gray-100">
+          <div className="hidden md:flex items-center border rounded-full px-3 py-1 bg-gray-100 mr-4">
             <svg className="w-5 h-5 text-gray-500 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
             <input type="text" placeholder="Search" className="bg-transparent outline-none text-gray-700 w-32" />
           </div>
@@ -34,7 +34,7 @@ const Navbar = () => {
           <a href="#" className="text-gray-500 hover:text-yellow-500">
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
           </a>
-          <div className="hidden md:flex space-x-6">
+          <div className="hidden md:flex space-x-4">
              <a href="#" className="text-gray-500 hover:text-yellow-500">Login</a>
           <a href="#" className="text-gray-500 hover:text-yellow-500">Sign Up</a>
           </div>
