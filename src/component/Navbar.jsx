7b49dedc8059cac21221c1f4a84f7483b184a1fe
AAlabs10 <Adeniranadetoyese25@gmail.com>
2025-09-10 02:08:50 -0700

    all

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -37,8 +37,8 @@ const Navbar = () => {
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
           </Link>
           <div className="hidden md:flex space-x-4">
-             <Link href="/loginpage" className="text-gray-500 hover:text-yellow-500">Login</Link>
-          <Link href="/signup" className="text-gray-500 hover:text-yellow-500">Sign Up</Link>
+             <Link to="/loginpage" className="text-gray-500 hover:text-yellow-500">Login</Link>
+          <Link to="/signup" className="text-gray-500 hover:text-yellow-500">Sign Up</Link>
           </div>
           
 
@@ -65,8 +65,8 @@ const Navbar = () => {
           <a href="#" className="block py-2 text-white hover:bg-yellow-500">Shop</a>
           <a href="#" className="block py-2 text-white hover:bg-yellow-500">About</a>
           <a href="#" className="block py-2 text-white hover:bg-yellow-500">Contact Us</a>
-          <Link href="/loginpage" className="block py-2 text-white hover:bg-yellow-500">Login</Link>
-          <Link href="/signup" className="block py-2 text-white hover:bg-yellow-500">Sign Up</Link>
+          <Link to="/loginpage" className="block py-2 text-white hover:bg-yellow-500">Login</Link>
+          <Link to="/signup" className="block py-2 text-white hover:bg-yellow-500">Sign Up</Link>
           {/* Mobile Search Input */}
           <div className="flex items-center border rounded-full px-3 py-1 bg-gray-100 mt-2">
             <svg className="w-5 h-5 text-gray-500 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
